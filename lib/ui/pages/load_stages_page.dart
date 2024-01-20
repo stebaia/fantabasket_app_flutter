8c690a7c6983ecfc91f960a021011c0ad7bad0bb
Emanuele Lamagna <elamagna@bsdsoftware.it>
2024-01-20 20:53:16 +0100

    Updated stage load page with mocked cards

--- lib/ui/pages/load_stages_page.dart
+++ lib/ui/pages/load_stages_page.dart
@@ -1,7 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fantabasket_app_flutter/bloc/create_team_bloc/create_team_bloc.dart';
+import 'package:fantabasket_app_flutter/ui/widgets/load_stage_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intl/intl.dart';
 
 class LoadStagesPage extends StatefulWidget with AutoRouteWrapper {
   const LoadStagesPage({super.key});
@@ -24,7 +26,6 @@ class LoadStagesPage extends StatefulWidget with AutoRouteWrapper {
 class _LoadStagesPageState extends State<LoadStagesPage> {
   @override
   Widget build(BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -43,40 +44,31 @@ class _LoadStagesPageState extends State<LoadStagesPage> {
               child: Text("Errore nel caricamento delle tappe"),
             );
           } else {
-            return SingleChildScrollView(
-              child: Container(
-                alignment: Alignment.center,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    ...(context.read<CreateTeamBloc>().state
-                            as ResultGetStagesState)
-                        .stagesList
-                        .stages!
-                        .map((stage) => SizedBox(
-                              width: width * 0.9,
-                              child: Card(
-                                elevation: 2,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        "${stage.city}",
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ))
-                  ],
+            return Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(color: Colors.grey),
                 ),
-              ),
+                Expanded(
+                  flex: 9,
+                  child: SingleChildScrollView(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 20),
+                      alignment: Alignment.center,
+                      child: Column(
+                        children: [
+                          ...(context.read<CreateTeamBloc>().state
+                                  as ResultGetStagesState)
+                              .stagesList
+                              .stages!
+                              .map((stage) => LoadStageCard(stage: stage))
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             );
           }
         },
