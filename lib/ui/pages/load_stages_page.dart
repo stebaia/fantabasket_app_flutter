e938e9babe44dfc8310f1f5f413bea5224db40af
Emanuele Lamagna <elamagna@bsdsoftware.it>
2024-01-20 22:11:55 +0100

    Added skeleton in stages loading

--- lib/ui/pages/load_stages_page.dart
+++ lib/ui/pages/load_stages_page.dart
@@ -1,9 +1,12 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:fantabasket_app_flutter/bloc/create_team_bloc/create_team_bloc.dart';
+import 'package:fantabasket_app_flutter/model/stage.dart';
 import 'package:fantabasket_app_flutter/ui/widgets/load_stage_card.dart';
 import 'package:fantabasket_app_flutter/ui/widgets/sponsors_banner.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:skeletonizer/skeletonizer.dart';
+
 import 'package:intl/intl.dart';
 
 class LoadStagesPage extends StatefulWidget with AutoRouteWrapper {
@@ -37,8 +40,36 @@ class _LoadStagesPageState extends State<LoadStagesPage> {
         listener: (context, state) {},
         builder: (context, state) {
           if (state is TryGetStagesState) {
-            return const Center(
-              child: CircularProgressIndicator(),
+            return Skeletonizer(
+              child: Column(
+                children: [
+                  const SponsorsBanner(),
+                  Expanded(
+                    flex: 9,
+                    child: SingleChildScrollView(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 20),
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            ...(List.filled(
+                                6,
+                                LoadStageCard(
+                                    stage: Stage(
+                                  "city",
+                                  "fieldName",
+                                  "address",
+                                  DateTime.now(),
+                                  DateTime.now(),
+                                  DateTime.now(),
+                                ))))
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             );
           } else if (state is ErrorGetStagesState) {
             return const Center(
