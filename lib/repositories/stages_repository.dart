7aab874df21b7cc7215c00efc0e71d9095741b79
Emanuele Lamagna <elamagna@bsdsoftware.it>
2024-01-20 19:12:57 +0100

    Created stages repository

---
+++ lib/repositories/stages_repository.dart
@@ -0,0 +1,62 @@
+import 'package:fantabasket_app_flutter/model/stage.dart';
+import 'package:fantabasket_app_flutter/model/stages_list.dart';
+
+class StagesRepository {
+  StagesRepository();
+
+  Future<StagesList> getStages() async {
+    return StagesList(
+      count: 3,
+      stages: [
+        Stage(
+          "Cesena",
+          "Campetto Case Finali",
+          "Via Cesare Balbo, 9, 47521 Cesena FC",
+          DateTime(2024, 3, 5, 21, 30),
+          DateTime(2024, 2, 5, 15, 00),
+          DateTime(2024, 3, 4, 15, 00),
+        ),
+        Stage(
+          "Bologna",
+          "Playground Meloncello",
+          "Via Porrettana, 1833, 40135 Bologna BO",
+          DateTime(2024, 3, 5, 21, 30),
+          DateTime(2024, 2, 5, 15, 00),
+          DateTime(2024, 3, 4, 15, 00),
+        ),
+        Stage(
+          "Forlì",
+          "Campetto Edelweiss",
+          "Viale Fratelli Spazzoli, 113, 47121 Forlì FC",
+          DateTime(2024, 3, 5, 21, 30),
+          DateTime(2024, 2, 5, 15, 00),
+          DateTime(2024, 3, 4, 15, 00),
+        ),
+        Stage(
+          "Cervia",
+          "Fantini Club",
+          "Lungomare Grazia Deledda, 182, 48015 Cervia RA",
+          DateTime(2024, 3, 5, 21, 30),
+          DateTime(2024, 2, 5, 15, 00),
+          DateTime(2024, 3, 4, 15, 00),
+        ),
+        Stage(
+          "Firenze",
+          "A.D. Dlf Firenze Basket",
+          "Via Giovanni Paisiello, 131, 50144 Firenze FI",
+          DateTime(2024, 3, 5, 21, 30),
+          DateTime(2024, 2, 5, 15, 00),
+          DateTime(2024, 3, 4, 15, 00),
+        ),
+        Stage(
+          "Imola",
+          "Grifo Basket",
+          "Via Alfredo Oriani, 2/4, 40026 Imola BO",
+          DateTime(2024, 3, 5, 21, 30),
+          DateTime(2024, 2, 5, 15, 00),
+          DateTime(2024, 3, 4, 15, 00),
+        ),
+      ],
+    );
+  }
+}
