51263d6acb564d38a1ca1d177bfdce00283cf5fc
Emanuele Lamagna <elamagna@bsdsoftware.it>
2024-03-01 11:44:02 +0100

    Added dynamic behavior to confirm button

--- lib/ui/widgets/completion_button.dart
+++ lib/ui/widgets/completion_button.dart
@@ -1,4 +1,7 @@
+import 'package:fantabasket_app_flutter/bloc/cubit/captain_cubit.dart/captain_cubit.dart';
+import 'package:fantabasket_app_flutter/bloc/cubit/sixth_man_cubit/sixth_man_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CompletionButton extends StatefulWidget {
   final TextEditingController controller;
@@ -13,21 +16,39 @@ class CompletionButton extends StatefulWidget {
 }
 
 class _CompletionButtonState extends State<CompletionButton> {
+  bool hasName = false;
+  bool hasCaptain = false;
+  bool hasSixthMan = false;
+
   @override
   Widget build(BuildContext context) {
     widget.controller.addListener(
-      () => setState(() {}),
-    );
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-          backgroundColor: widget.controller.text != ""
-              ? const Color.fromARGB(255, 225, 135, 57)
-              : const Color.fromARGB(255, 208, 201, 195)),
-      onPressed: widget.controller.text != "" ? () {} : null,
-      child: Text(
-        "Conferma squadra",
-        style: TextStyle(
-          color: widget.controller.text != "" ? Colors.white : Colors.grey,
+        () => setState(() => hasName = widget.controller.text != ""));
+
+    var enabled = hasName && hasCaptain && hasSixthMan;
+
+    return BlocListener<SixthManCubit, SixthManState>(
+      listener: (BuildContext context, state) {
+        setState(
+            () => hasSixthMan = context.read<SixthManCubit>().sixthMan != null);
+      },
+      child: BlocListener<CaptainCubit, CaptainState>(
+        listener: (BuildContext context, state) {
+          setState(
+              () => hasCaptain = context.read<CaptainCubit>().captain != null);
+        },
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: enabled
+                  ? const Color.fromARGB(255, 225, 135, 57)
+                  : const Color.fromARGB(255, 208, 201, 195)),
+          onPressed: widget.controller.text != "" ? () {} : null,
+          child: Text(
+            "Conferma squadra",
+            style: TextStyle(
+              color: enabled ? Colors.white : Colors.grey,
+            ),
+          ),
         ),
       ),
     );
