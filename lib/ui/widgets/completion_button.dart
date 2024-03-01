42c0f5ea9272222dca874c24a815b4e4dcb797d6
Emanuele Lamagna <elamagna@bsdsoftware.it>
2024-03-01 20:20:10 +0100

    Finished completion button logic

--- lib/ui/widgets/completion_button.dart
+++ lib/ui/widgets/completion_button.dart
@@ -1,5 +1,6 @@
 import 'package:fantabasket_app_flutter/bloc/cubit/captain_cubit.dart/captain_cubit.dart';
 import 'package:fantabasket_app_flutter/bloc/cubit/sixth_man_cubit/sixth_man_cubit.dart';
+import 'package:fantabasket_app_flutter/bloc/select_player_bloc/select_player_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -17,38 +18,27 @@ class CompletionButton extends StatefulWidget {
 
 class _CompletionButtonState extends State<CompletionButton> {
   bool hasName = false;
-  bool hasCaptain = false;
-  bool hasSixthMan = false;
 
   @override
   Widget build(BuildContext context) {
     widget.controller.addListener(
         () => setState(() => hasName = widget.controller.text != ""));
 
-    var enabled = hasName && hasCaptain && hasSixthMan;
+    var captain = context.watch<CaptainCubit>();
+    var sixth = context.watch<SixthManCubit>();
 
-    return BlocListener<SixthManCubit, SixthManState>(
-      listener: (BuildContext context, state) {
-        setState(
-            () => hasSixthMan = context.read<SixthManCubit>().sixthMan != null);
-      },
-      child: BlocListener<CaptainCubit, CaptainState>(
-        listener: (BuildContext context, state) {
-          setState(
-              () => hasCaptain = context.read<CaptainCubit>().captain != null);
-        },
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: enabled
-                  ? const Color.fromARGB(255, 225, 135, 57)
-                  : const Color.fromARGB(255, 208, 201, 195)),
-          onPressed: enabled ? () {} : null,
-          child: Text(
-            "Conferma squadra",
-            style: TextStyle(
-              color: enabled ? Colors.white : Colors.grey,
-            ),
-          ),
+    var enabled = hasName && captain.captain != null && sixth.sixthMan != null;
+
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          backgroundColor: enabled
+              ? const Color.fromARGB(255, 225, 135, 57)
+              : const Color.fromARGB(255, 208, 201, 195)),
+      onPressed: enabled ? () async {} : null,
+      child: Text(
+        "Conferma squadra",
+        style: TextStyle(
+          color: enabled ? Colors.white : Colors.grey,
         ),
       ),
     );
