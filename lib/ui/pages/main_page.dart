cb237f886257b8d27cfe2ea613e993427ec7a877
Emanuele Lamagna <elamagna@bsdsoftware.it>
2024-04-19 23:56:26 +0200

    Updated app splash screen

--- lib/ui/pages/main_page.dart
+++ lib/ui/pages/main_page.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:fantabasket_app_flutter/bloc/cubit/auth_cubit/auth_cubit.dart';
 import 'package:fantabasket_app_flutter/routes/app_router.gr.dart';
 import 'package:fantabasket_app_flutter/utils/theme_helper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -15,23 +16,71 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocListener<AuthCubit, AuthState>(
+    return Scaffold(
+      body: BlocListener<AuthCubit, AuthState>(
         listener: (context, state) {
           if (state is AuthenticatedState) {
             _replacePage(context, const CoreRoute());
           } else {
-            _replacePage(context, LoginRoute());
+            _replacePage(context, const LoginRoute());
           }
         },
-        child: const Center(
-          child:
-              CircularProgressIndicator(color: ThemeHelper.enabledPrimaryColor),
-        ));
+        child: Container(
+          color: Theme.of(context).colorScheme.secondary,
+          child: Stack(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                      color: Theme.of(context).colorScheme.secondary,
+                      height: double.infinity,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      color: Colors.white,
+                      height: double.infinity,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 8,
+                    child: Container(
+                      color: Theme.of(context).colorScheme.tertiary,
+                      height: double.infinity,
+                    ),
+                  ),
+                ],
+              ),
+              Center(
+                child: Container(
+                  padding: const EdgeInsets.all(40.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.asset(
+                    'assets/images/basketballicon.png',
+                    height: 150,
+                    color: Theme.of(context).colorScheme.tertiary,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 
   Future<void> _replacePage(
       BuildContext context, PageRouteInfo routeInfo) async {
     //context.router.popUntilRoot();
-    await context.router.push(routeInfo);
+    await Future.delayed(const Duration(seconds: 2));
+    if (context.mounted) {
+      await context.router.push(routeInfo);
+    }
   }
 }
